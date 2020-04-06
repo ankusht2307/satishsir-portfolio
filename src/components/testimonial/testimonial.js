f7e29c3c0f23cf1521cc890e086b4b186812649e
Ankush Thakur <ankush@bonamisoftware.com>
2020-04-07 01:00:56 +0530

    testimonial section initial check-in

--- src/components/testimonial/testimonial.js
+++ src/components/testimonial/testimonial.js
@@ -6,60 +6,56 @@ import Dp from '../images/dp.png';
 
 function Testimonial() {
     return (
-        <div className="testimonial_wrapper">
+        <div className="testimonial-wrapper">
             <h1 className="text-center">
                 Testimonials
             </h1>
-            <Row className="testimonial">
-                <Col xs={3} sm={4} md={4} lg={4} xl={4} className="test_dp text-right">
-                    <Image src={Dp} alt="test_image" fluid/>
-                </Col>
-                <Col xs={9} sm={8} md={8} lg={6} xl={6} className="test_content">
-                    <p>
+            <Row className="testimonial text-center">
+                <Col xs={10} sm={10} md={10} lg={8} xl={6} className="testimonial-col">
+                    <Image src={Dp} alt="test-image" fluid />
+                    <p className="pt-3 ml-4">
                         Laborum duis eiusmod ad exercitation. Nisi ex commodo ullamco consectetur
                         exercitation consectetur officia pariatur. Nisi deserunt tempor sit tempor
                         laboris sint dolore exercitation reprehenderit fugiat voluptate duis sint.
                         Consectetur aliqua Lorem aliquip adipisicing cillum irure est. Duis
-                </p>
+                    </p>
+
                 </Col>
             </Row>
-            <Row className="testimonial">
-                <Col xs={3} sm={4} md={4} lg={4} xl={4} className="test_dp text-right">
-                    <Image src={Dp} alt="test_image" fluid/>
-                </Col>
-                <Col xs={9} sm={8} md={8} lg={6} xl={6} className="test_content">
-                    <p>
+            <Row className="testimonial text-center">
+                <Col xs={10} sm={10} md={10} lg={8} xl={6} className="testimonial-col">
+                    <Image src={Dp} alt="test-image" fluid />
+                    <p className="pt-3 ml-4">
                         Laborum duis eiusmod ad exercitation. Nisi ex commodo ullamco consectetur
                         exercitation consectetur officia pariatur. Nisi deserunt tempor sit tempor
                         laboris sint dolore exercitation reprehenderit fugiat voluptate duis sint.
                         Consectetur aliqua Lorem aliquip adipisicing cillum irure est. Duis
-                </p>
+                    </p>
+
                 </Col>
             </Row>
-            <Row className="testimonial">
-                <Col xs={3} sm={4} md={4} lg={4} xl={4} className="test_dp text-right">
-                    <Image src={Dp} alt="test_image" fluid/>
-                </Col>
-                <Col xs={9} sm={8} md={8} lg={6} xl={6} className="test_content">
-                    <p>
+            <Row className="testimonial text-center">
+                <Col xs={10} sm={10} md={10} lg={8} xl={6} className="testimonial-col">
+                    <Image src={Dp} alt="test-image" fluid />
+                    <p className="pt-3 ml-4">
                         Laborum duis eiusmod ad exercitation. Nisi ex commodo ullamco consectetur
                         exercitation consectetur officia pariatur. Nisi deserunt tempor sit tempor
                         laboris sint dolore exercitation reprehenderit fugiat voluptate duis sint.
                         Consectetur aliqua Lorem aliquip adipisicing cillum irure est. Duis
-                </p>
+                    </p>
+
                 </Col>
             </Row>
-            <Row className="testimonial">
-                <Col xs={3} sm={4} md={4} lg={4} xl={4} className="test_dp text-right">
-                    <Image src={Dp} alt="test_image" fluid/>
-                </Col>
-                <Col xs={9} sm={8} md={8} lg={6} xl={6} className="test_content">
-                    <p>
+            <Row className="testimonial text-center">
+                <Col xs={10} sm={10} md={10} lg={8} xl={6} className="testimonial-col">
+                    <Image src={Dp} alt="test-image" fluid />
+                    <p className="pt-3 ml-4">
                         Laborum duis eiusmod ad exercitation. Nisi ex commodo ullamco consectetur
                         exercitation consectetur officia pariatur. Nisi deserunt tempor sit tempor
                         laboris sint dolore exercitation reprehenderit fugiat voluptate duis sint.
                         Consectetur aliqua Lorem aliquip adipisicing cillum irure est. Duis
-                </p>
+                    </p>
+
                 </Col>
             </Row>
         </div>
