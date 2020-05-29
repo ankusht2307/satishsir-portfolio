f5d064d4308d56842b52dabfd45254db20cd8fb1
Pintu Singh <pintu@bonamisoftware.com>
2020-05-29 14:37:16 +0530

    Theme applied.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,14 +5,13 @@ import { Button } from 'react-bootstrap';
 import Dp from '../images/sir.png';
 
 function Header() {
-
     return (
         <div className="header" id="header">
             <Row className="header-content">
                 <Col xs={11} sm={10} md={5} lg={5} xl={5} className="text">
                     <div>
                         <h1 className="h-1">Hi I am</h1>
-                        <h2 className="h-2">Satish pandey</h2>
+                        <h2 className="h-2">Satish Pandey</h2>
                         <h3 className="h-3">A Professional Full Stack Developer</h3>
                         <div>
                             <a href="#services">
